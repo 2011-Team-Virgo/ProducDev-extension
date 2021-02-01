74ffbc74dba0b87fd08d60ca71c51e31b413c78b
Cwlowe <calvinwolfelowe@gmail.com>
2021-02-01 10:08:16 -0800

    Delete dbConfig.js
    Remove dbConfig.js

--- db/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = {
-  apiKey: "AIzaSyAe9TREuTK9UgMRKcibsnRPZnH7VRIQqZE'",
-  authDomain: 'producdev-1277b.firebaseapp.com',
-  databaseURL: 'https://producdev-1277b-default-rtdb.firebaseio.com/',
-  storageBucket: 'producdev-1277b.appspot.com',
-};
-
-module.exports = dbConfig;
