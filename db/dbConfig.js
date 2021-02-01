023d8b90871c97689aa2611d356646adfa08bb22
Kerri Ryan <kerri.ryan11@gmail.com>
2021-02-01 11:44:25 -0500

    fix: hid db config

---
+++ db/dbConfig.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+  apiKey: "AIzaSyAe9TREuTK9UgMRKcibsnRPZnH7VRIQqZE'",
+  authDomain: 'producdev-1277b.firebaseapp.com',
+  databaseURL: 'https://producdev-1277b-default-rtdb.firebaseio.com/',
+  storageBucket: 'producdev-1277b.appspot.com',
+};
+
+module.exports = dbConfig;
--- db/index.js
+++ db/index.js
@@ -1,13 +1,7 @@
 const firebase = require('firebase/app');
 const auth = require('firebase/auth');
 const db = require('firebase/database');
-
-const dbConfig = {
-  apiKey: "AIzaSyAe9TREuTK9UgMRKcibsnRPZnH7VRIQqZE'",
-  authDomain: 'producdev-1277b.firebaseapp.com',
-  databaseURL: 'https://producdev-1277b-default-rtdb.firebaseio.com/',
-  storageBucket: 'producdev-1277b.appspot.com',
-};
+const dbConfig = require('./dbConfig');
 
 firebase.initializeApp(dbConfig);
 
