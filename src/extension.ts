276d5037131053dd9fecf49b84b03885c3820657
eficek <eficek@protonmail.com>
2021-02-01 14:30:36 -0600

    feat: record time spent in editor+keystrokes made

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "producdev" is now active!');
+  console.log('Congratulations, your extension "helloworld2" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
+  interface LooseObject {
+    [key: string]: any;
+  }
+
+  let time: LooseObject = {};
+  let currentFile: string = "";
+  let timestamp: number = -1;
+
+  let keystrokes: LooseObject = {};
+
   let disposable = vscode.commands.registerCommand(
-    "producdev.helloWorld",
+    "helloworld2.helloWorld",
     () => {
-      // The code you place here will be executed every time your command is executed
+      vscode.window.onDidChangeActiveTextEditor((event) => {
+        vscode.window.showInformationMessage("active editor changed");
+        if (timestamp > 0 && currentFile) {
+          if (time[currentFile]) {
+            time[currentFile] += Date.now() - timestamp;
+          } else {
+            time[currentFile] = Date.now() - timestamp;
+          }
+        }
+        timestamp = Date.now();
+        if (typeof event?.document.fileName === "string") {
+          currentFile = event?.document.fileName;
+        }
+      });
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from ProducDev!");
+      vscode.workspace.onDidChangeTextDocument((event) => {
+        if (currentFile) {
+          if (keystrokes[currentFile]) {
+            keystrokes[currentFile]++;
+          } else {
+            keystrokes[currentFile] = 1;
+          }
+        }
+      });
     }
   );
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
