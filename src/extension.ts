4e43e1da312be6c6079a341bd17a3eb902753d88
eficek <eficek@protonmail.com>
2021-02-04 16:36:44 -0600

    feat: return metric payload on 30 min interval

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const dateFormat = require("dateformat");
 import * as vscode from "vscode";
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -10,25 +12,52 @@ export function activate(context: vscode.ExtensionContext) {
     [key: string]: any;
   }
 
-  let time: LooseObject = {};
+  let pkg: LooseObject = { name: "PROJECT_NAME_UNKNOWN" };
+
+  let time: LooseObject;
+  let keystrokes: LooseObject;
   let currentFile: string = "";
   let timestamp: number = -1;
+  let projectPath: string = "";
 
-  let keystrokes: LooseObject = {};
+  if (vscode.workspace.workspaceFolders) {
+    projectPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    if (fs.existsSync(`${projectPath}/package.json`)) {
+      pkg = JSON.parse(fs.readFileSync(`${projectPath}/package.json`, "utf8"));
+    } else {
+      vscode.window.showErrorMessage(
+        "ERROR: Your package.json file could not be found in the open directory"
+      );
+    }
+  }
 
+  const setup = () => {
+    time = {};
+    keystrokes = {};
+    timestamp = Date.now(); // this sets the timestamp and filename to start tracking time upon opening vs code
+    if (typeof vscode.window.activeTextEditor?.document.fileName === "string") {
+      currentFile = vscode.window.activeTextEditor?.document.fileName.slice(
+        projectPath.length
+      );
+    }
+  };
+  setup();
+
+  const updateTime = () => {
+    if (time[currentFile]) {
+      time[currentFile] += Date.now() - timestamp;
+    } else {
+      time[currentFile] = Date.now() - timestamp;
+    }
+  };
   vscode.window.onDidChangeActiveTextEditor((event) => {
     if (timestamp > 0 && currentFile) {
-      if (time[currentFile]) {
-        time[currentFile] += Date.now() - timestamp;
-      } else {
-        time[currentFile] = Date.now() - timestamp;
-      }
+      updateTime();
     }
     timestamp = Date.now();
     if (typeof event?.document.fileName === "string") {
-      currentFile = event?.document.fileName;
+      currentFile = event?.document.fileName.slice(projectPath.length);
     }
-    console.log(time);
   });
 
   vscode.workspace.onDidChangeTextDocument((event) => {
@@ -40,6 +69,56 @@ export function activate(context: vscode.ExtensionContext) {
       }
     }
   });
+
+  const payload = () => {
+    const ts = dateFormat(new Date(), "yyyy-mm-dd_h:MM:ss");
+    let res: LooseObject = {
+      [pkg.name]: { seconds: {}, keystrokes: {} },
+      timestamp: ts,
+    };
+    for (const file in time) {
+      if (Object.prototype.hasOwnProperty.call(time, file)) {
+        if (!res[pkg.name].seconds[ts]) {
+          res[pkg.name].seconds[ts] = 0;
+        }
+        res[pkg.name].seconds[ts] += Math.floor(time[file] / 1000);
+      }
+    }
+    for (const file in keystrokes) {
+      if (Object.prototype.hasOwnProperty.call(keystrokes, file)) {
+        if (!res[pkg.name].keystrokes[ts]) {
+          res[pkg.name].keystrokes[ts] = 0;
+        }
+        res[pkg.name].keystrokes[ts] += keystrokes[file];
+      }
+    }
+
+    // returns a payload sorted by file - possible future use?
+    /*for (const file in time) {
+      if (Object.prototype.hasOwnProperty.call(time, file)) {
+        if (!res[pkg.name][file]) {
+          res[pkg.name][file] = { time: 0, keystrokes: 0 };
+        }
+        res[pkg.name][file].time += time[file];
+      }
+    }
+    for (const file in keystrokes) {
+      if (Object.prototype.hasOwnProperty.call(keystrokes, file)) {
+        if (!res[pkg.name][file]) {
+          res[pkg.name][file] = { time: 0, keystrokes: 0 };
+        }
+        res[pkg.name][file].keystrokes += keystrokes[file];
+      }
+    }*/
+    return res;
+  };
+
+  setInterval(() => {
+    // run every 30 mins
+    updateTime();
+    console.log(payload());
+    setup();
+  }, 180000);
 }
 
 export function deactivate() {}
