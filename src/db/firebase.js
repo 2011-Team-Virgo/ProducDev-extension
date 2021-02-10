6a2707c4090227217613b55f463bfe4bcb25a209
eficek <eficek@protonmail.com>
2021-02-10 16:32:02 -0600

    fix: remove useless if statement

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -3,12 +3,10 @@ import axios from "axios";
 export const firebaseUpload = async (pl, projectName) => {
   try {
     for (const file in pl[projectName]) {
-      if (pl[projectName].hasOwnProperty(file)) {
-        await axios.patch(
-          `https://producdev-1277b-default-rtdb.firebaseio.com/users/${pl.id}/projects/${projectName}/${file}.json`,
-          pl[projectName][file]
-        );
-      }
+      await axios.patch(
+        `https://producdev-1277b-default-rtdb.firebaseio.com/users/${pl.id}/projects/${projectName}/${file}.json`,
+        pl[projectName][file]
+      );
     }
   } catch (error) {
     console.log(error);
